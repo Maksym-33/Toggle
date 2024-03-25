15e247976dd3e435aa97bf54a4575c053c50e36f
Maksym-33 <164895788+Maksym-33@users.noreply.github.com>
2024-03-25 09:02:59 -0600

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,33 @@
+const heading = document.getElementById('myHeading');
+const toggleColourButton = document.getElementById('toggleColourButton')
+
+toggleColourButton.addEventListener('click',toggleColourClicked);
+
+function toggleColourClicked(){
+    let buttonText = toggleColourButton.textContent;
+
+    if (buttonText === 'Red'){
+        heading.classList.remove('red');
+        heading.classList.add('green');
+        toggleColourButton.textContent ='Green';
+
+    }else if (buttonText === 'Green'){
+        heading.classList.remove('green');
+        heading.classList.add('blue');
+        toggleColourButton.textContent = 'Blue';
+
+    }else if (buttonText === 'Blue'){
+        heading.classList.remove('blue');
+        heading.classList.add('red');
+        toggleColourButton.textContent = 'Red';
+    }
+}
+
+toggleColourButton.addEventListener('Click',toggleFontClicked);
+
+function toggleFontClicked(){
+
+    let buttonFont = toggleFontButton.textContent;
+
+    if (buttonText
+}
